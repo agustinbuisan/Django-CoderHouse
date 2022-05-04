8e3a7c1d5e8bc5af999ac11b3487f09f4a5e3678
agustinbuisan <agustin.buisan@gmail.com>
2022-05-04 17:54:03 -0300

    Clase20

---
+++ AppCoder/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', inicio, name='inicio'),
+    
+   # path('creaCurso', curso),
+    
+    path('cursos/', cursos, name='cursos'),
+    path('profesores/', profesores, name='profesores'),
+    path('estudiantes/', estudiantes, name='estudiantes'),
+    path('entregables/', entregables, name='entregables'),
+    
+
+]
